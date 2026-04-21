d5787ee2ca1ade1eb2db4e9bf8ce7a6c9242bbdb
Raspreet Singh <singhraspreet921@gmail.com>
2026-04-21 14:08:36 +0530

    Voice selection

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,8 @@
 import { baseProcedure, createTRPCRouter } from "../init";
+import { voicesRouter } from "./voices";
+
 export const appRouter = createTRPCRouter({
-  health: baseProcedure.query(async () => {
-    return { status: "OK" };
-  }),
+  voices: voicesRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
